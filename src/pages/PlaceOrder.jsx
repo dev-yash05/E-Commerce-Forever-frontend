c756f1365b803e1d61628a7d3e111426b65f9d40
Yash Namdeo <yashnamdeo754@gmail.com>
2025-05-24 19:13:10 +0530

    completed cart functionality and placeorder page half

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Title from '../components/Title'
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* -----------Left side ------------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
+        <div className="text-xl sm:text-2xl my-3">
+          <Title text1={'DELIVERY'} text2={'INFORMATION'} />
+        </div>
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='First Name' type="text" />
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Last Name' type="text" />
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Email Address' type="email" />
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Street' type="email" />
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='City' type="text" />
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='State' type="text" />
+        </div>
+        <div className="flex gap-3">
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Zipcode' type="number" />
+          <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Country' type="text" />
+        </div>
+        <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' placeholder='Phone' type="number" />
+      </div>
+      
+    </div>
   )
 }
 
